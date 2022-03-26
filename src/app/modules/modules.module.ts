a401e11cee0f022890e20782a1d1615376a29e15
Vitor Bianchi <41026988+VitorBianchi@users.noreply.github.com>
2022-03-26 02:55:19 -0300

    Refactor

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { HomeModule } from './home-module/home.module';
-import { PageModule } from './page-module/docs.module';
+import { PageModule } from './page-module/page.module';
 
 @NgModule({
   declarations: [],
